b6c402ad39df40ebd69d02555366927488d66d4d
Kid-Justice <drew.jennings@outlook.com>
2020-03-10 17:13:49 -0700

    brugh

--- Put ur stuff here/Drew/Items/HighAccessKeyCard.c
+++ Put ur stuff here/Drew/Items/HighAccessKeyCard.c
@@ -1,13 +1,13 @@
 /******************************************************************************
-filename    AllAccessKeyCardFunctions.c
+filename    HighAccessKeyCardFunctions.c
 author      Drew Jennings
 
 Brief Description:
-This file defines the functions to create a specific item, the "AllAccessKeyCard".
+This file defines the functions to create a specific item, the "HighAccessKeyCard".
 
 ******************************************************************************/
 #include "stdafx.h" /* UNREFERENCED_PARAMETER, NULL*/
-#include "AllAccessKeyCardFunctions.h" /* Function declarations */
+#include "HighAccessKeyCardFunctions.h" /* Function declarations */
 #include "GameState.h" /* struct GameState, GameState_ChangeScore */
 #include "GameFlags.h" /* GameFlags_IsInList */
 #include "WorldData.h" /* WorldData_GetRoom */
@@ -16,25 +16,25 @@ This file defines the functions to create a specific item, the "AllAccessKeyCard
 #include "Item.h" /* Item_Create */
 
 
-/* Helper: The action performed when the AllAccessKeyCard is taken. */
-void AllAccessKeyCard_Take(CommandContext context, GameState* gameState, WorldData* worldData)
+/* Helper: The action performed when the HighAccessKeyCard is taken. */
+void HighAccessKeyCard_Take(CommandContext context, GameState* gameState, WorldData* worldData)
 {
 	/* avoid W4 warnings on unused parameters - this function conforms to a function typedef */
 	UNREFERENCED_PARAMETER(context);
 	UNREFERENCED_PARAMETER(gameState);
 	UNREFERENCED_PARAMETER(worldData);
 
-	/* Give the user a hint about how the AllAccessKeyCard might be used, whenever it is picked up. */
+	/* Give the user a hint about how the HighAccessKeyCard might be used, whenever it is picked up. */
 	printf("It can unlock any door...\n");
 }
 
 
-/* Helper: The action performed when the AllAccessKeyCard is used. */
-void AllAccessKeyCard_Use(CommandContext context, GameState* gameState, WorldData* worldData)
+/* Helper: The action performed when the HighAccessKeyCard is used. */
+void HighAccessKeyCard_Use(CommandContext context, GameState* gameState, WorldData* worldData)
 {
 	Room* room; /* The current room */
 	ItemList** roomItemsPtr; /* The list of items in the current room */
-	Item* AllAccessKeyCard; /* The AllAccessKeyCard in the player's inventory */
+	Item* HighAccessKeyCard; /* The HighAccessKeyCard in the player's inventory */
 
 	/* safety check on the parameters */
 	if ((gameState == NULL) || (worldData == NULL))
@@ -42,20 +42,20 @@ void AllAccessKeyCard_Use(CommandContext context, GameState* gameState, WorldDat
 		return; /* take no action if the parameters are invalid */
 	}
 
-	/* check if the user is using the AllAccessKeyCard out of their inventory */
+	/* check if the user is using the HighAccessKeyCard out of their inventory */
 	if (context != CommandContext_Item_Inventory)
 	{
-		/* the user doesn't have the AllAccessKeyCard - inform the user of the problem and take no action */
-		printf("You must have the All Access Key Card before you can use it.\n");
+		/* the user doesn't have the HighAccessKeyCard - inform the user of the problem and take no action */
+		printf("You must have the High Access Key Card before you can use it.\n");
 		return;
 	}
 
 
 	/* check if the cage has already been broken and scored */
-	if (GameFlags_IsInList(gameState->gameFlags, "usedAllAccessKeyCard"))
+	if (GameFlags_IsInList(gameState->gameFlags, "usedHighAccessKeyCard"))
 	{
-		/* the player already used the AllAccessKeyCard - inform the user of the problem and take no action */
-		printf("You already used the All Access Key Card here.\n");
+		/* the player already used the HighAccessKeyCard - inform the user of the problem and take no action */
+		printf("You already used the High Access Key Card here.\n");
 		return;
 	}
 	else
@@ -70,14 +70,14 @@ void AllAccessKeyCard_Use(CommandContext context, GameState* gameState, WorldDat
 			return; /* take no action, as something is wrong - we should always have an item list */
 		}
 
-		/* Find the AllAccessKeyCard in the player's inventory - it should be there, since we are in the Inventory context */
-		AllAccessKeyCard = ItemList_FindItem(gameState->inventory, "AllAccessKeyCard");
+		/* Find the HighAccessKeyCard in the player's inventory - it should be there, since we are in the Inventory context */
+		HighAccessKeyCard = ItemList_FindItem(gameState->inventory, "HighAccessKeyCard");
 
-		/* Remove the AllAccessKeyCard from the user's inventory - they won't need it again */
-		gameState->inventory = ItemList_Remove(gameState->inventory, AllAccessKeyCard);
+		/* Remove the HighAccessKeyCard from the user's inventory - they won't need it again */
+		gameState->inventory = ItemList_Remove(gameState->inventory, HighAccessKeyCard);
 
 		/* Tell the user what they did */
-		printf("You used the All Access Key Card\nAll doors are now unlocked.\n");
+		printf("You used the High Access Key Card\nDoors with a High Access level are now unlocked.\n");
 
 		/* Add to the player's score */
 		GameState_ChangeScore(gameState, 10);
@@ -89,14 +89,14 @@ void AllAccessKeyCard_Use(CommandContext context, GameState* gameState, WorldDat
 		/**roomItemsPtr = ItemList_Add(*roomItemsPtr, Egg_Build());*/
 
 		/* the gold piece has not been scored, so mark the flag */
-		gameState->gameFlags = GameFlags_Add(gameState->gameFlags, "UsedAllAccessKeyCard");
+		gameState->gameFlags = GameFlags_Add(gameState->gameFlags, "UsedHighAccessKeyCard");
 	}
 }
 
 
-/* Build a "AllAccessKeyCard" object */
-Item* AllAccessKey_Build()
+/* Build a "HighAccessKeyCard" object */
+Item* HighAccessKey_Build()
 {
-	/* Create a "AllAccessKeyCard" item, using the functions defined in this file */
-	return Item_Create("AllAccessKeyCard", "A Key Card that can be used to open any door.", true, AllAccessKeyCard_Use, AllAccessKeyCard_Take, NULL);
+	/* Create a "HighAccessKeyCard" item, using the functions defined in this file */
+	return Item_Create("HighAccessKeyCard", "A Key Card that can be used to open high access doors.", true, HighAccessKeyCard_Use, HighAccessKeyCard_Take, NULL);
 }
---
+++ Put ur stuff here/Drew/Items/HighAccessKeyCard.h
@@ -0,0 +1,15 @@
+/******************************************************************************
+filename    HighAccessKeyCardFunctions.h
+author      Drew Jennings
+
+Brief Description:
+This file declares the functions to create a specific item, the "HighAccessKeyCard".
+
+******************************************************************************/
+#pragma once
+
+typedef struct Item Item;
+
+
+/* Build a "AllAccessKeyCard" object */
+Item* HighAccessKeyCard_Build();
